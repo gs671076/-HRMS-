12621e24e0f4ca05965c70abe4bc2529acc5016a
Gaurav Soni <gs671076@gmail.com>
2026-06-05 20:28:22 +0530

    Enums

---
+++ hrms/src/main/java/com/hrms/enums/Designation.java
@@ -0,0 +1,5 @@
+package com.hrms.enums;
+
+public enum Designation {
+    MASON, ELECTRICIAN, PLUMBER, SUPERVISOR, HELPER
+}
---
+++ hrms/src/main/java/com/hrms/enums/SettlementStatus.java
@@ -0,0 +1,5 @@
+package com.hrms.enums;
+
+public enum SettlementStatus {
+    PENDING, SETTLED
+}
